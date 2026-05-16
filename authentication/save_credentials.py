28ad9e700a7f1e87b428ba1de48b8ed63ee2e7a5
dps44-alu <dps44@alu.ua.es>
2026-05-16 13:58:25 +0200

    authentication removed from ignore

---
+++ authentication/save_credentials.py
@@ -0,0 +1,8 @@
+from qiskit_ibm_runtime import QiskitRuntimeService
+ 
+QiskitRuntimeService.save_account(
+token="ACwZ6JubKb9jZ84F4pR3dr6wZ9F8ZEzMFWku4fFqPoN2",
+instance="crn:v1:bluemix:public:quantum-computing:us-east:a/a1c319a37d214a7299755981fcdb2e8f:f99de395-af80-40d1-a035-73224dff1efe::",
+overwrite=True,
+set_as_default = True,
+)
